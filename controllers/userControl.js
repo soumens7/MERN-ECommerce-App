a78565752c49477094cf45a761898948b2408e1e
soumen sarkar <soumensarkar@soumens-MacBook-Pro.local>
2025-02-03 00:20:32 +0530

    added conditions for user credentials

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -6,14 +6,16 @@ const userControl = {
     try {
       //console.log("Request Body:", req.body); // Debugging line
       const { name, email, password } = req.body;
-        if (!name || !email || !password) {
-            return res.status(400).json({ msg: "Please fill in all fields." });
-        }
+      if (!name || !email || !password) {
+        return res.status(400).json({ msg: "Please fill in all fields." });
+      }
       const user = await Users.findOne({ email });
       if (user) return res.status(400).json({ msg: "Email already exists." });
 
       if (password.length < 6) {
-        return res.status(400).json({ msg: "Password must be at least 6 characters long." });
+        return res
+          .status(400)
+          .json({ msg: "Password must be at least 6 characters long." });
       }
 
       const salt = await bcrypt.genSalt(10);
