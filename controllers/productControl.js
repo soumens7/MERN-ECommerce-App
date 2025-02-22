ec276b15db9128e784b741c51b5f7c6bbc7376dd
soumen sarkar <soumensarkar7498@gmail.com>
2025-02-22 21:18:39 +0530

    Initial commit for server side

--- controllers/productControl.js
+++ controllers/productControl.js
@@ -62,7 +62,7 @@ class APIfeatures {
    */
   paginating() {
     const page = this.queryString.page * 1 || 1; // Convert page to a number (default: 1)
-    const limit = this.queryString.limit * 1 || 10; // Convert limit to a number (default: 9)
+    const limit = this.queryString.limit * 1 || 10; // Convert limit to a number (default: 10)
     const skip = (page - 1) * limit; // Calculate the number of documents to skip
 
     this.query = this.query.skip(skip).limit(limit); // Apply pagination
